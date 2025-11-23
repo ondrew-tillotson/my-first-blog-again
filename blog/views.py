7800029a2f195bea353a299ac1ef46897d674b1b
ondrew-tillotson <52128842+ondrew-tillotson@users.noreply.github.com>
2025-11-22 18:19:21 -0600

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post # Assuming you have a Post model
 def post_list(request):
-  posts = Post.objects.all() # Fetch all posts from the database
+  posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+  #posts = Post.objects.all() # Fetch all posts from the database
   return render(request, 'blog/post_list.html', {'posts': posts})
 # Create your views here.
