2f912dc75eaad0f81ee4e244c711c2d5df2bf250
ondrew-tillotson <52128842+ondrew-tillotson@users.noreply.github.com>
2025-11-23 14:23:29 -0600

    First commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+#no i don't want this to actually be code
+
+
+"""
+yo, 
+wazzup
+Hannah
+"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+
 ]
