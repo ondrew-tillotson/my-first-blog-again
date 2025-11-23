e692751eee5b61eee6a0d1bccf0a415fb234b952
ondrew-tillotson <52128842+ondrew-tillotson@users.noreply.github.com>
2025-11-22 18:20:01 -0600

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Post # Assuming you have a Post model
 def post_list(request):
   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
